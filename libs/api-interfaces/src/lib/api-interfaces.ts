dc4e93d2880dc99e9f671c4bea67493975e8266a
Kelechi Berquist <koberquist@gmail.com>
2022-12-31 16:31:56 -0500

    feat: add api controller for google maps api key

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export interface Message {
   message: string;
+}
+export interface MapApiResponse {
+  apiKey: string;
+}
+
+export class MapApiResponseConcrete implements MapApiResponse {
+  @ApiProperty()
+  apiKey: string,
 }
