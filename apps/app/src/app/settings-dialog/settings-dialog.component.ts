69df7deb735f0beb924530742343504efc5f6f04
Kelechi Berquist <koberquist@gmail.com>
2023-02-10 19:35:52 -0500

    feat: adds internationalization

--- apps/app/src/app/settings-dialog/settings-dialog.component.ts
+++ apps/app/src/app/settings-dialog/settings-dialog.component.ts
@@ -5,6 +5,7 @@ import { Store } from '@ngrx/store';
 import { tap } from 'rxjs';
 import { updateSettings } from '../store/settings.actions';
 import { selectSettingsState } from '../store/settings.selectors';
+import { minimalTimezoneSet } from './timezones';
 
 export interface Settings {
   language: FormControl<string>;
@@ -23,10 +24,7 @@ export class SettingsDialogComponent implements OnInit {
     { name: 'English', value: 'en-GB' },
   ];
 
-  timezones = [
-    { name: 'US Eastern Time (New York)', value: 'America/New_York' },
-    { name: 'West African Time (Lagos)', value: 'Africa/Lagos' },
-  ];
+  timezones = minimalTimezoneSet;
 
   form = new FormGroup<Settings>({
     language: new FormControl('', {
