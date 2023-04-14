83d722cd91cb5394adfcf71260210cc472edf447
KelechiBerquist <kelechi@berquist.tech>
2023-04-14 18:50:23 -0400

    feat: refines skeleton for admin app

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'powerdey-root',
+  selector: 'pw-admin-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
