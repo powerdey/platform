32ea35de5715b9e518e15ed9bfd144f08c2896a1
KelechiBerquist <kelechi@berquist.tech>
2023-04-14 19:22:30 -0400

    feat: sets up login and dashboard module

--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+];
