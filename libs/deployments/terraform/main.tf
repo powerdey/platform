db943eda89bd5ffd336d137b2a99ca585362bba2
Azuka Okuleye <azuka@zahymaka.com>
2023-09-01 14:35:20 -0700

    feat: setup bigquery via terraform (#28)
    * feat: setup bigquery via terraform
    
    closes #26
    
    * chore: fix workflow
    
    * chore: fix workflow x2
    
    * chore: add apply step

---
+++ libs/deployments/terraform/main.tf
@@ -0,0 +1,74 @@
+data "google_project" "default" {}
+
+module "bigquery" {
+  source  = "terraform-google-modules/bigquery/google"
+  version = "~> 6.1"
+
+  dataset_id                  = "power_records"
+  dataset_name                = "power_records"
+  description                 = "Data synced from firestore records"
+  project_id                  = data.google_project.default.project_id
+  location                    = "US"
+  default_table_expiration_ms = 8.64e+7
+
+  tables = [
+    {
+      table_id = "trends",
+      schema = jsonencode([
+        {
+          name : "on",
+          type : "BOOL",
+          mode : "REQUIRED",
+          description : "Power on"
+        },
+        {
+          name : "recorded_at",
+          type : "DATETIME",
+          mode : "REQUIRED",
+          description : "Date record was made"
+        },
+        {
+          name : "device_id",
+          type : "STRING",
+          mode : "REQUIRED",
+          description : "Unique device id"
+        }
+      ]),
+      time_partitioning = {
+        type                     = "DAY",
+        field                    = null,
+        require_partition_filter = false,
+        expiration_ms            = null,
+      },
+      range_partitioning = null,
+      expiration_time    = null,
+      clustering         = [],
+      labels             = {},
+    }
+  ]
+
+  #  views = [
+  #    {
+  #      view_id    = "barview",
+  #      use_legacy_sql = false,
+  #      query = <<EOF
+  #      SELECT
+  #       column_a,
+  #       column_b,
+  #      FROM
+  #        `project_id.dataset_id.table_id`
+  #      WHERE
+  #        approved_user = SESSION_USER
+  #      EOF,
+  #      labels = {
+  #        env      = "devops"
+  #        billable = "true"
+  #        owner    = "joedoe"
+  #      }
+  #    }
+  #  ]
+  dataset_labels = {
+    env      = "dev"
+    billable = "true"
+  }
+}
